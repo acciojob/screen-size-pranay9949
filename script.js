846a97b642d7f2f6830fd0bd4491d44ecb25d1aa
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-09 10:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let a=window.innerWidth;
+let d=window.innerHeight;
+let b=document.createElement("h1");
+b.id="sizeInfo";
+b.textContent=`Width: ${a} and Height: ${d}`
+document.body.appendChild(b);
